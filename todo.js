b9f4317774d3d60fb970537d837a3f1b21122559
Dipali-32 <104363937+Dipali-32@users.noreply.github.com>
2024-02-13 22:45:10 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,161 @@
+const taskInput = document.querySelector(".task-input input"),
+    filters = document.querySelectorAll(".filters span"),
+    clearAll = document.querySelector(".clear-btn"),
+    taskBox = document.querySelector(".task-box");
+
+let editId,
+    isEditTask = false,
+    todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });
+});
+
+showTodo("all");
+
+function showTodo(filter) {
+    let liTag = "";
+    if (todos) {
+        todos.forEach((todo, id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if (filter == todo.status || filter == "all") {
+                liTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }
+        });
+    }
+    taskBox.innerHTML =
+        liTag || `<span>You don't have any task here</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length
+        ? clearAll.classList.remove("active")
+        : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300
+        ? taskBox.classList.add("overflow")
+        : taskBox.classList.remove("overflow");
+}
+
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", (e) => {
+        if (e.target.tagName != "I" || e.target != selectedTask) {
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if (selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+
+function deleteTask(deleteId, filter) {
+    isEditTask = false;
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(filter);
+}
+
+clearAll.addEventListener("click", () => {
+    isEditTask = false;
+    todos.splice(0, todos.length);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
+});
+
+taskInput.addEventListener("keyup", (e) => {
+    let userTask = taskInput.value.trim();
+    if (e.key == "Enter" && userTask) {
+        if (!isEditTask) {
+            todos = !todos ? [] : todos;
+            let taskInfo = { name: userTask, status: "pending" };
+            todos.push(taskInfo);
+        } else {
+            isEditTask = false;
+            todos[editId].name = userTask;
+        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo(document.querySelector("span.active").id);
+    }
+});
+
+//calender
+const daysTag= document.querySelector(".days"),
+currentDate = document.querySelector(".current-date"),
+prevNextIcon = document.querySelectorAll(".icons span");
+// getting new date, current year and month
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+// storing full name of all months in array
+const months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+              const renderCalendar = () => {
+                let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+                lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+                lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+                lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+                let liTag = "";
+                for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+                    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+                }
+                for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+                    // adding active class to li if the current day, month, and year matched
+                    let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                                 && currYear === new Date().getFullYear() ? "active" : "";
+                    liTag += `<li class="${isToday}">${i}</li>`;
+                }
+                for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+                    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+                }
+                currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+                daysTag.innerHTML = liTag;
+            }
+            renderCalendar();
+            prevNextIcon.forEach(icon => { // getting prev and next icons
+                icon.addEventListener("click", () => { // adding click event on both icons
+                    // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+                    currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+                    if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+                        // creating a new date of current year & month and pass it as date value
+                        date = new Date(currYear, currMonth, new Date().getDate());
+                        currYear = date.getFullYear(); // updating current year with new date year
+                        currMonth = date.getMonth(); // updating current month with new date month
+                    } else {
+                        date = new Date(); // pass the current date as date value
+                    }
+                    renderCalendar(); // calling renderCalendar function
+                });
+            });
